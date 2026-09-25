6bdadf001577cd9da63c54ad96d40343913eface
NGUYENHUUTINH-uit <huutinhpc.2023@gmail.com>
2026-09-25 20:14:00 +0700

    chore: Update swagger print output

--- backend/src/main/java/com/shoppingagent/ShoppingAgentApplication.java
+++ backend/src/main/java/com/shoppingagent/ShoppingAgentApplication.java
@@ -15,8 +15,8 @@ public class ShoppingAgentApplication {
     @org.springframework.context.event.EventListener(org.springframework.boot.context.event.ApplicationReadyEvent.class)
     public void printSwaggerUrl() {
         System.out.println("\n---------------------------------------------------------");
-        System.out.println("🚀 Backend đã khởi động thành công!");
-        System.out.println("👉 CLICK VÀO ĐÂY ĐỂ TEST SWAGGER: http://localhost:8080/api/swagger-ui.html");
+        System.out.println("Backend đã khởi động thành công!");
+        System.out.println("CLICK VÀO ĐÂY ĐỂ TEST SWAGGER: http://localhost:8080/api/swagger-ui.html");
         System.out.println("---------------------------------------------------------\n");
     }
 }
